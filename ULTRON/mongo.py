32bfa74c7b2cc205c9766e0825da6f317793df70
RISHABH ANANAD <94896142+thanosuser@users.noreply.github.com>
2022-03-23 08:46:04 -0700

    Update mongo.py

--- ULTRON/mongo.py
+++ ULTRON/mongo.py
@@ -7,15 +7,15 @@ from ULTRON import MONGO_DB_URI
 
 
 MONGO_PORT = "27017"
-MONGO_DB_URI = "mongodb+srv://ULTRONx:rishabh123classmate@cluster0.hx2qi.mongodb.net/ULTRONx?retryWrites=true&w=majority"
-MONGO_DB = "ULTRONRobot"
+MONGO_DB_URI = "mongodb+srv://lucyx:rishabh123classmate@cluster0.hx2qi.mongodb.net/lucyx?retryWrites=true&w=majority"
+MONGO_DB = "lucyx"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, 27017)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, 27017)
 db = motor[MONGO_DB]
-db = client["ULTRONRobot"]
+db = client["lucyx"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:

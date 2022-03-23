b99e38917b91ea9efaaf6e563daac0d673a92347
thanosuser <thanosuserbot@gmail.com>
2022-03-22 23:33:52 -0700

    first commit

---
+++ THANOS-main/ULTRON/modules/alive.py
@@ -0,0 +1,24 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from ULTRON.events import register
+from ULTRON import telethn as tbot
+
+
+RISHABH = "kissme"
+
+@register(pattern=("/ULTRON"))
+async def awake(event):
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm ULTRON bot.** \n\n"
+  TEXT += "💞 **ULTRON IS ALIVE** \n\n"
+  TEXT += f"💞 **Owner : [RISHABH](https://t.me/ULTRON_OWNER)** \n\n"
+  TEXT += f"💞 **Library Version :** `{telever}` \n\n"
+  TEXT += f"💞 **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"💞 **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "**Thanks For using ULTRON 💞**"
+  BUTTON = [[Button.url("Help", "https://t.me/ULTRON_MANAGER_bot?start=help"), Button.url("Support", "https://t.me/ULTRON_BOTS")]]
+  await tbot.send_file(event.chat_id, RISHABH, caption=TEXT,  buttons=BUTTON)
